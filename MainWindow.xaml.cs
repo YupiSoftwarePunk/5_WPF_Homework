033a6fb72afdaaa7e6aad12d2d3b7226d3bdbbd6
Yupi <youngperchik2404@gmail.com>
2025-05-28 23:17:41 +0500

    Created repo and started 5 homework

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_WPF_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ToDo> todo = new List<ToDo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            todo.Add(new ToDo("Дописать проект", new DateTime(2025, 5, 23), "Доделать проект с математическими классами"));
+            ToDolist.ItemsSource = todo;
+        }
+
+
+        private void btnAdd(object sender, RoutedEventArgs e)
+        {
+            var NewTodo = new NewTodo();
+            NewTodo.Show();
+            NewTodo.Owner = this;
+        }
+
+
+        private void btnDelete(object sender, RoutedEventArgs e)
+        {
+            if (ToDolist.SelectedItem as ToDo == null)
+            {
+                return;
+            }
+            else
+            {
+                todo.Remove((ToDo)ToDolist.SelectedItem);
+
+                ToDolist.ItemsSource = null;
+                ToDolist.ItemsSource = todo;
+            }
+        }
+
+
+        public class ToDo
+        {
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+            public string Description { get; set; }
+
+            public ToDo(string name, DateTime date, string description)
+            {
+                Name = name;
+                Date = date;
+                Description = description;
+            }
+        }
+    }
+}
---
+++ NewTodo.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static _5_WPF_Homework.MainWindow;
+
+namespace _5_WPF_Homework
+{
+    /// <summary>
+    /// Логика взаимодействия для NewTodo.xaml
+    /// </summary>
+    public partial class NewTodo : Window
+    {
+        public NewTodo()
+        {
+            InitializeComponent();
+
+            dateToDo.SelectedDate = DateTime.Now;
+        }
+
+
+        private void ToDoSave(object sender, RoutedEventArgs e)
+        {
+            if (dateToDo.SelectedDate == null)
+            {
+                MessageBox.Show("Выберите дату!");
+                return;
+            }
+            var main = (MainWindow)Owner;
+
+            var temp = dateToDo.SelectedDate.Value;
+            main.todo.Add(new ToDo(titleToDo.Text, temp, descriptionToDo.Text));
+
+            main.ToDolist.ItemsSource = null;
+            main.ToDolist.ItemsSource = main.todo;
+
+            Hide();
+        }
+    }
+}
